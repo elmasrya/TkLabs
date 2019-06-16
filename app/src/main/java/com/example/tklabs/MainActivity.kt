97c8e6ebd16efa90884130bcdce6e04ce79f3617
Andrew El Masry <andrewelmasry@GRBRMBP01.attlocal.net>
2019-06-16 10:47:13 -0400

    hey

---
+++ app/src/main/java/com/example/tklabs/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tklabs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
